fbafd1cf6ff98b19e053d3584e6513484de2c002
PRATIK GITTE <96045097+PRATIKGITTE@users.noreply.github.com>
2025-11-28 15:02:20 +0530

    Update install.sh

--- backend/install.sh
+++ backend/install.sh
@@ -2,14 +2,17 @@
 
 apt-get update
 
-# Install Python and pip
+# Install Python + pip
 apt-get install -y python3 python3-pip
 
-# Install yt-dlp (VERY IMPORTANT)
+# Install JS runtime for yt-dlp (VERY IMPORTANT)
+pip install py-mini-racer
+
+# Install yt-dlp
 pip install -U yt-dlp
 
-# Install chrome for puppeteer
+# Install chromium for puppeteer
 apt-get install -y chromium-browser
 
-# Give execution permission
+# Give permission
 chmod +x /opt/render/project/src/backend/*.py
