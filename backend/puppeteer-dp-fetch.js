3f8a58be1e0abc5bbcb4bbb9324bebd90c3ab691
PRATIK GITTE <96045097+PRATIKGITTE@users.noreply.github.com>
2025-11-28 14:30:08 +0530

    Update puppeteer-dp-fetch.js

--- backend/puppeteer-dp-fetch.js
+++ backend/puppeteer-dp-fetch.js
@@ -6,7 +6,11 @@ const fs = require('fs');
   const number = process.argv[2];
   if (!number) return console.error('Missing number');
 
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+  headless: true,
+  args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+});
+
   const page = await browser.newPage();
 
   try {
